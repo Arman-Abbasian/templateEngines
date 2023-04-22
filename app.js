7b76de9aa0bbaf92f40e5a20ac7c2d7cd5ec96fe
Arman Abbasian <abasian.arman@gmail.com>
2023-04-22 23:37:29 +0430

    work on website with hbs

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express=require("express");
 const path=require("path");
+const hbs=require("hbs");
 const { NotFoundError, ErrorHandler } = require("./public/js/utils");
-const { users } = require("./db");
+const { header,navList } = require("./db");
 const app=express();
+hbs.registerPartials(path.join(__dirname,"views/hbs/partials"))
 app.use(express.static("public"))
 app.set("view engine","hbs");
 app.set("views",path.join(__dirname,"views"))
 
 app.get("/",(req,res,next)=>{
-    res.render("hbs/index.hbs",{users})
+    res.render("hbs/index.hbs",{header,navList})
 });
 
 app.use(NotFoundError);
