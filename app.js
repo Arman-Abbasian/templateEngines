ed4304c7f38f5e3e051ab8f3acd255289f50595e
Arman Abbasian <abasian.arman@gmail.com>
2023-04-22 21:00:59 +0430

    make project with hbs

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const path=require("path");
 const { NotFoundError, ErrorHandler } = require("./public/js/utils");
 const { users } = require("./db");
 const app=express();
-app.use("/static",express.static("public"))
-app.set("view engine","pug");
+app.use(express.static("public"))
+app.set("view engine","hbs");
 app.set("views",path.join(__dirname,"views"))
 
 app.get("/",(req,res,next)=>{
-    res.render("index",{users})
+    res.render("hbs/index.hbs",{users})
 });
-
-
-
 
 app.use(NotFoundError);
 app.use(ErrorHandler);
